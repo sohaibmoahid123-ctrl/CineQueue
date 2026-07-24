fdf92dd8aed10a7776ff0bd597b560089e4944f0
sohaibmoahid123-ctrl <sohaibmoahid123@gmail.com>
2026-07-24 18:38:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -414,6 +414,15 @@ function renderMovieDetail(id) {
               <span>${movie.durationMinutes} min</span>
             </div>
             <p class="detail-synopsis">${movie.synopsis}</p>
+          <div class="server-bar">
+            <span>سرور پخش:</span>
+            <button class="srv-btn active" onclick="changeServer('https://vidsrc.to/embed/movie/${movie.id}', this)">سرور اصلی</button>
+            <button class="srv-btn" onclick="changeServer('https://vidsrc.pro/embed/movie/${movie.id}', this)">سرور کمکی</button>
+          </div>
+
+          <div class="player-wrapper" style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; border-radius: 10px; margin-bottom: 20px;">
+            <iframe id="main-player" src="https://vidsrc.to/embed/movie/${movie.id}" style="position: absolute; top:0; left:0; width:100%; height:100%; border:none;" allowfullscreen></iframe>
+          </div>
 
             <div class="detail-credits">
               <div class="credit-row">
