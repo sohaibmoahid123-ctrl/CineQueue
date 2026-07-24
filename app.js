4985adac88b72dc73aececac6616de810fdc4a01
sohaibmoahid123-ctrl <sohaibmoahid123@gmail.com>
2026-07-24 21:45:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -462,6 +462,11 @@ function renderMovieDetail(id) {
             </p>`
           : ''}
       </div>
+{/* AFTER */}
+<div className="mt-auto bg-[#...] p-6 rounded-2xl ...">
+  <h3>Download Video</h3>
+  {/* Buttons */}
+</div>
 
         <!-- More in this genre -->
         ${related.length > 0 ? `
