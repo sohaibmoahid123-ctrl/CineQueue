ada8961d3cdd985a83593afb4c5c829f87ff4f09
sohaibmoahid123-ctrl <sohaibmoahid123@gmail.com>
2026-07-24 22:04:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -427,7 +427,11 @@ function renderMovieDetail(id) {
           </div>
         </div>
 
+</div>
+</div>
+
 <div class="download-section">
+
         <h3 class="download-heading">
           <svg width="20" height="20" viewBox="0 0 24 24" fill="none"
                stroke="currentColor" stroke-width="2.5"
