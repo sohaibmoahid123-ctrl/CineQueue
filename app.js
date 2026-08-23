97f717192270cd3ffc48450d40a1211ba4cf9adf
sohaibmoahid123-ctrl <sohaibmoahid123@gmail.com>
2026-08-23 13:54:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // ============================================================
 import { API_KEY, BASE_URL, IMAGE_URL, YTS_API_URL } from './js/config.js';
 import { handleNewServerDownload } from './js/decryptor.js';
+import { handleYTSDownload, getAutoDownloadLinks } from './js/yts.js';
 
 const app = document.getElementById('app');
 const adScript = document.createElement('script');
