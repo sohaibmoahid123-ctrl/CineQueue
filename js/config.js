17a1fac68d6b7d4e515d131166dee572e085cdd1
sohaibmoahid123-ctrl <sohaibmoahid123@gmail.com>
2026-08-23 12:13:38 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// ============================================================
+// config.js  —  تنظیمات اصلی سایت CineQueue
+// ============================================================
+
+export const API_KEY = 'cab1be6caea88ea79b1101c13ddb5702';
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+export const YTS_API_URL = 'https://movies-api.accel.li/api/v2';
